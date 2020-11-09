6d6ad890c595da00a6b94425cb49ac0bc8e30ca6
yu <fukanougo@gmail.com>
2020-11-09 23:19:10 +0900

    認証情報を変更

--- login/index.php
+++ login/index.php
@@ -1,18 +1,20 @@
 <?php
 include('../include/php/voiceroom_base.php');
-require ("../private/auth.php");//データベース接続のための情報
+require ("../private/auth.php");
 $err_msg = "";
 if (isset($_POST['login'])) {
   $username = $_POST['username'];
   $password = $_POST['password'];
   try {
-    $dbh = new PDO($dsn,$user,$password,$options);
-    $sql = 'select * from' . $db['dbname'] . '.koeroom_db where username = :name';
-    $stmt = $dbh->prepare($sql);
+    $db = new PDO("mysql:host=" . $server . "; dbname=".$database."; charset=utf8", $user, $pass );
+    echo "1";
+    $sql = 'select * from koeroom_db where username = :name';
+    $stmt = $db->prepare($sql);
     $stmt->execute(array(':name' => $username));
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    echo "1";
     $stmt = null;
-    $dbh = null;
+    $db = null;
     echo $password;
     echo password_verify($password, $result['password']);
     if(password_verify($password, $result['password'])){
@@ -31,15 +33,14 @@ if (isset($_POST['login'])) {
 ?>
 <!doctype html>
 <html lang="ja">
-
 <head>
-  <?php include('../include/php/head.php'); ?>
+  <?php include( $_SERVER['DOCUMENT_ROOT'] . '/include/php/head.php'); ?>
 </head>
 
 <body>
   
   <div id="include-header">
-    <?php include('../include/php/header.php'); ?>
+    <?php include( $_SERVER['DOCUMENT_ROOT'] . '/include/php/header.php'); ?>
   </div>
     <div class="container">
         <div class="columns is-mobile is-centered  is-marginless">
